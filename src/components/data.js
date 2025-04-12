f63a50bb34c6180ed2f5ece134081639ac6d55e2
Thanaboon Nantadusit <thanaboonnantadusit@Thanaboons-iMac.local>
2025-04-12 09:35:02 +0700

    fix bug

--- src/components/data.js
+++ src/components/data.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 export const fetchUser = async () => {
   try {
-    const response = await axios.get(https://reqres.in/api/users?page=2);
-    const data = response.data
+    const response = await axios.get("https://reqres.in/api/users?page=2");
+    return response.data;
   } catch (error) {
-    console.error("Error fetching comments:", error);
+    console.error("Error fetching users:", error);
   }
 };
