39488b58b98b1f6136b3692359b7d7cb82765d1d
Thanaboon Nantadusit <thanaboonnantadusit@Thanaboons-iMac.local>
2025-04-12 10:12:11 +0700

    clean app

--- src/App.jsx
+++ src/App.jsx
@@ -21,14 +21,12 @@ function App() {
 
   return (
     <div>
-      <h1>React ใช้ .jsx ได้! 🚀</h1>
-      <p>ลองเปลี่ยน App.js เป็น App.jsx แล้ว!</p>
       <p>ฝึก push pull ที่นี่</p>
       <ul>
         {user.map((eachUser) => (
           <li key={eachUser.id}>
             <p>{eachUser.first_name}</p>
-            <img src={eachUser.avatar} alt="profile picture"/>
+            <img src={eachUser.avatar} alt="profile picture" />
           </li>
         ))}
       </ul>
