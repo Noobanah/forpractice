f4f091d6cc178ae46ea98a53edbb6df1fed315af
Thanaboon Nantadusit <thanaboonnantadusit@Thanaboons-iMac.local>
2025-04-12 09:08:06 +0700

    for push

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React ใช้ .jsx ได้! 🚀</h1>
+      <p>ลองเปลี่ยน App.js เป็น App.jsx แล้ว!</p>
     </div>
   );
 }
